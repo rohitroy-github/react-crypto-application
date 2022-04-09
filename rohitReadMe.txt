fff5679a8f1143bcb3c8b1ae3afcb649b92841d7
rohit <rohitdevops.roy@gmail.com>
2022-04-10 01:36:43 +0530

    rohitReadMe.txt addition alongside updates

---
+++ rohitReadMe.txt
@@ -0,0 +1,13 @@
+./components
+CoinDetails.jsx checked 
+Exchanges.jsx checked 
+Navbar.jsx checked
+News.jsx checked
+Loader.jsx checked
+index.js checked
+Homepage.jsx checked
+
+
+./services checked
+cryptoNewsApi.js checked
+cryptoApi.js checked 
--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,10 +1,3 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
-
-const cryptoApiHeaders = {
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': 'eaef008ba9msh5ec15ec633cae10p15671ajsn1a5b3a0406d2'
-
-}
 // const options = {
 //     method: 'GET',
 //     url: 'https://coinranking1.p.rapidapi.com/coins',
@@ -17,5 +10,46 @@ const cryptoApiHeaders = {
 //       limit: '50',
 //       offset: '0'
 //     },
-/
 //   };
+
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// Note: Change v1 to v2 on rapid api
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': 'eaef008ba9msh5ec15ec633cae10p15671ajsn1a5b3a0406d2'
+};
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+
+    // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+    }),
+
+    // Note: To access this endpoint you need premium plan
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetExchangesQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
